4a5666b94da367897bd5a39576d90bc99a0fc97d
halwala <halwalajanani@gmail.com>
2024-05-16 18:27:55 +0530

    Updated car.py and program.py

--- program.py
+++ program.py
@@ -1,5 +1,10 @@
 from car import Car
 
-car = Car('A')
-print('Hello')
-print(car.value)
+def main():
+    my_car = Car("Toyota", "Camry", 2022)
+    print(f"My car is a {my_car.get_descriptive_name()}.")
+
+    my_car.read_odometer()
+
+if __name__ == "__main__":
+    main()
